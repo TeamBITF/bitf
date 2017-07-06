cc4e6b2713338bfad2acd23e92d4bb7924e76c3f
kagi810 <tyu_mom@yahoo.co.jp>
2017-07-06 16:34:30 +0900

    g

---
+++ src/net/BITF/panel/Test.java
@@ -0,0 +1,20 @@
+package net.BITF.panel;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class Test extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Test() {
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, 95, 244, 165);
+		add(lblNewLabel);
+
+	}
+}
