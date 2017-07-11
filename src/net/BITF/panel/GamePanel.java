746b116e5a8beaf58e34e992f4df58d454632c5a
kagi810 <tyu_mom@yahoo.co.jp>
2017-07-11 18:02:53 +0900

    gamepanel でやる

--- src/net/BITF/panel/GamePanel.java
+++ src/net/BITF/panel/GamePanel.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
@@ -27,12 +29,14 @@ public class GamePanel extends BITFPanel implements ActionListener{
 	private AnswerPanel answerPanel;
 	private MainPanel testPanel;
 	private StatusPanel statusPanel;
-
+	private JComboBox comboBox;
+	
 //	private JPanel layoutPanelH;
 //	private JPanel layoutPanelV;
 
 	private JLabel warp;
-
+	private JLabel stu;
+	
 	protected Timer timer;
 	protected int totalTimeLimit;
 	private int time;
@@ -60,25 +64,36 @@ public class GamePanel extends BITFPanel implements ActionListener{
 //		layoutPanelV.setLayout(new BoxLayout(layoutPanelV, BoxLayout.X_AXIS));
 
 
-		answerPanel = new AnswerPanel(this);
-		answerPanel.setBounds(0, 0,this.getWidth() , this.getHeight());
-		answerPanel.setLocation(100,50);
+//		answerPanel = new AnswerPanel(this);
+//		answerPanel.setBounds(0, 0,this.getWidth() , this.getHeight());
+//		answerPanel.setLocation(100,50);
 		
 		result = 2;
 
 		testPanel = new MainPanel(result);
-		//testPanel.setBounds(0, 0, this.getWidth(), this.getHeight());	//表示サイズを設定
-		testPanel.setLocation(10,50);
+		testPanel.setLocation(10,80);
 		
-		statusPanel = new StatusPanel(this);
-		//statusPanel.setBounds(0, 0, statusPanel.getWidth(), statusPanel.getHeight());
-		statusPanel.setLocation(10,50);
+//		statusPanel = new StatusPanel(this);
+//		statusPanel.setBounds(0, 0, statusPanel.getWidth(), statusPanel.getHeight());
+//		statusPanel.setLocation(0,0);
 		
 		ImageIcon icon = new ImageIcon("resource/data/Game/雷.jpg");
 		warp = new JLabel(icon);
 		warp.setBounds(0,0,icon.getIconWidth(),icon.getIconHeight());
 
-
+		ImageIcon btn1 = new ImageIcon("resource/data/Start/startbutton.png");
+		JButton button1 = new JButton(btn1);
+	    button1.setBounds(900, 540, 150,60 );
+	    
+	    JComboBox combo = new JComboBox();
+	    combo.setBounds(900, 80, 200,40 );
+	    
+	    ImageIcon icon2 =new ImageIcon("resource/data/Game/gauge_frame.gif");
+	    stu = new JLabel(icon2);
+	    stu.setBounds(10, 10, icon2.getIconWidth(),icon2.getIconHeight() );
+	    
+	    
+		
 		//BoxLayout
 //		layoutPanelH.add(statusPanel);
 //		layoutPanelH.add(testPanel);
@@ -90,9 +105,12 @@ public class GamePanel extends BITFPanel implements ActionListener{
 
 		
 		add(testPanel);
-		add(answerPanel);
-		add(statusPanel);
-				
+		
+//		add(answerPanel);
+//		add(statusPanel);
+		add(button1);
+		add(combo);
+		add(stu);
 		add(warp);
 
 
@@ -149,7 +167,7 @@ public class GamePanel extends BITFPanel implements ActionListener{
 			//1枚にかける時間
 			if (time > 0){
 				time--;
-				statusPanel.updateUI();
+			//	statusPanel.updateUI(); 
 			}
 			else {
 				System.out.println("Timed out");
