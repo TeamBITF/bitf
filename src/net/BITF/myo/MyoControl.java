01729bd4f225f5f440737adbfcd79aae07cf1c87
kagi810 <tyu_mom@yahoo.co.jp>
2017-07-12 17:46:53 +0900

    myo処理

--- src/net/BITF/myo/MyoControl.java
+++ src/net/BITF/myo/MyoControl.java
@@ -12,6 +12,7 @@ public class MyoControl {
 
 	private DataCollector dataCollector;
 
+	private int x=600,y=600;//マウス初期位置
 	public MyoControl(){
 		/*
 		 * Myoの初期化
@@ -22,13 +23,27 @@ public class MyoControl {
 		dataCollector = new DataCollector();
 		hub.addListener(dataCollector);
 
+
+		myocon(); //実処理
 	}
 
 	public void update(){
 		hub.run(1000/20);
 	}
-
 	public DataCollector getDataCollector(){
 		return dataCollector;
+	}
+	public void myocon(){
+//		try{
+//			Robot r = new Robot();  //インスタンス生成
+//			  r.mouseMove(x,y ); //座標(mouseX, mouseY)にカーソルを移動
+//			}catch(AWTException e){
+//			  e.printStackTrace();
+//			}
+//
+//		if (roll_w < 8 && 4 < roll_w ){ //右
+//			x++;                       // 右へ移動
+//		}
+
 	}
 }
