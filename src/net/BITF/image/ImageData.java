b640753f3a6d98d2118bc79d796650c2d8b165a0
fms <keunes411.bitf@gmail.com>
2017-07-13 15:45:21 +0900

    軽量化

--- src/net/BITF/image/ImageData.java
+++ src/net/BITF/image/ImageData.java
@@ -16,7 +16,20 @@ public class ImageData {
 
 		System.out.println("Lead for \"" + fileName + "\"");
 		try {
-			setImage(ImageIO.read(new File("resource/data/game/background/" + fileName)));
+			BufferedImage raw = ImageIO.read(new File("resource/data/game/background/" + fileName));
+			int w = raw.getWidth();
+			int h = raw.getHeight();
+
+			int[] array = raw.getRGB(0, 0, w, h, null, 0, w);
+
+//			//α値を0にする
+//			for (int i = 0; i < array.length; i++){
+//				array[i] = array[i] & 0xFFFFFF | 0x22000000;
+//			}
+
+			image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+			image.setRGB(0, 0, w, h, array, 0, w);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -36,5 +49,22 @@ public class ImageData {
 
 	public void setImage(BufferedImage image) {
 		this.image = image;
+	}
+
+	public int getImageWidth(){
+		return image.getWidth();
+	}
+
+	public int getImageHeight(){
+		return image.getHeight();
+	}
+
+	public int[] getRGBArray(){
+		return image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+	}
+
+	public int[] getRGBArray(int startX, int startY, int w, int h){
+		return image.getRGB(startX, startY, w, h, null, 0, w);
+
 	}
 }
