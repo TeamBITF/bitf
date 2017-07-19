3622ba3b76045464dc3d06069b0fa0ffc018c31a
fms <keunes411.bitf@gmail.com>
2017-07-19 19:39:29 +0900

    BGM

--- src/net/BITF/util/BGMManager.java
+++ src/net/BITF/util/BGMManager.java
@@ -1,69 +1,203 @@
 package net.BITF.util;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 
 public class BGMManager implements Runnable{
 
-	Thread thread;
+	private String filename;
 
-	private AudioFormat audioFormat;
-	private SourceDataLine line;
-	private byte[] data;
+    private AudioInputStream audioStream = null;
+    private AudioFormat audioFormat = null;
+    private SourceDataLine sourceLine = null;
 
-	public boolean play;
+    private volatile boolean stopFlag = false;
+    private volatile boolean pauseFlag = false;
+    private volatile boolean isPlayingFlag = false;
+    private volatile float volume_dB = 0.0f;
 
-	public BGMManager(){
+    public BGMManager()
+    {
+    }
 
-		play = false;
+    public BGMManager(String filename)
+    {
+    	this.filename = filename;
+    }
 
-		try{
-			AudioInputStream sta =
-					AudioSystem.getAudioInputStream(new File("Z:/0楠祭　鍵班/BGM/famipop3.wav"));
-			data = new byte [sta.available()];
-			sta.read(data);
-			sta.close();
-			// ファイルのフォーマットを調べる
-			audioFormat = sta.getFormat();
+    /**
+     * @param filename the name of the file that is going to be played
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     * @throws FileNotFoundException
+     * @throws LineUnavailableException
+     */
+    public void playSound() throws FileNotFoundException, UnsupportedAudioFileException, IOException, LineUnavailableException
+    {
+    	isPlayingFlag = true;
 
+        if (filename.toLowerCase().endsWith(".txt"))
+        {
+        	System.out.println("Text Files Not Supported!");
+        }
+        else //if (filename.toLowerCase().endsWith(".mp3") || filename.toLowerCase().endsWith(".ogg"))
+        {
 
-			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        	final URL fileurl = ResourceLoader.instance.getResource(filename);
+            final AudioInputStream in = AudioSystem.getAudioInputStream(fileurl);
 
-			line = (SourceDataLine)AudioSystem.getLine(info);
+            final AudioFormat baseFormat = in.getFormat();
+            audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                    baseFormat.getSampleRate(),
+                    16,
+                    baseFormat.getChannels(),
+                    baseFormat.getChannels() * 2,
+                    baseFormat.getSampleRate(),
+                    false);
+            //System.out.println("Channels : " + baseFormat.getChannels());
+            audioStream = AudioSystem.getAudioInputStream(audioFormat, in);
+            final byte[] data = new byte[4096];
+            try{
+                SourceDataLine res = null;
+                DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+                res = (SourceDataLine) AudioSystem.getLine(info);
+                res.open(audioFormat);
+                sourceLine = res;
+                //System.out.println("Entering ...");
 
+                // Start
+                onPlay();
+                sourceLine.start();
+                int nBytesRead = 0;// nBytesWritten = 0;
+                while ((nBytesRead != -1) && (!stopFlag))
+                {
+                	if(!pauseFlag)
+                	{
+                		isPlayingFlag = true;
+	                    nBytesRead = audioStream.read(data, 0, data.length);
+	                    if (nBytesRead != -1)
+	                    {
+	                    	if (sourceLine.isControlSupported(FloatControl.Type.MASTER_GAIN))
+	                    	{
+	                            ((FloatControl) sourceLine.getControl(FloatControl.Type.MASTER_GAIN)).setValue(volume_dB);
+	                        }
+	                    	sourceLine.write(data, 0, nBytesRead);
+	                        //nBytesWritten = sourceLine.write(data, 0, nBytesRead);
+	                        //System.out.println("... -->" + data[0] + " bytesWritten:" + nBytesWritten);
+	                    }
+                	}
+                	else
+                	{
+                		isPlayingFlag = false;
+                	}
+                }
+                //System.out.println("Done ...");
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+                // Stop
+                sourceLine.drain();
+                sourceLine.stop();
+                sourceLine.close();
+                audioStream.close();
+            }catch(LineUnavailableException e){
+                e.printStackTrace();
+            }
+            in.close();
 
-		thread = new Thread(this);
-		thread.start();
+            isPlayingFlag = false;
+            onStop();
 
-	}
-
-	public void BGMStart(){
-		play = true;
-	}
+        }
+    }
 
 	@Override
-	public void run() {
-		while (true){
-			if (play){
-				try {
-					line.open(audioFormat);
-					line.start();
-					line.write(data, 0, data.length);
-				} catch (LineUnavailableException e) {
-					System.err.println("そんな曲ねぇぞ");
-				}
-			}
+	public void run()
+	{
+		stopFlag = false;
+		pauseFlag = false;
+
+		try {
+			playSound();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	public void play()
+	{
+		run();
+	}
+
+	public void onPlay(){}
+
+	public void pause()
+	{
+		pauseFlag = true;
+		onPause();
+	}
+
+	public void onPause(){}
+
+	public void resume()
+	{
+		pauseFlag = false;
+		onResume();
+	}
+
+	public void onResume(){}
+
+	public void stop()
+	{
+		stopFlag = true;
+	}
+
+	public void onStop(){}
+
+	public boolean isPlaying()
+	{
+		return isPlayingFlag;
+	}
+
+	public boolean isPaused()
+	{
+		return pauseFlag;
+	}
+
+	public void setFile(String filename)
+    {
+    	this.filename = filename;
+    }
+
+	public void setVolume(Float volume)
+	{
+		volume = volume==null ? 1.0F : volume;
+		volume = volume<=0.0F ? 0.0001F : volume;
+		setVolumeInDecibels((float)( 20.0*(Math.log(volume)/Math.log(10.0)) ));
+	}
+
+	public void setVolumeInDecibels(Float decibels)
+	{
+		decibels = decibels==null ? 0.0F : decibels;
+		this.volume_dB = decibels;
 	}
 }
